6476dbc156345afa76eac6f0cea24feb018ee273
Marcio Xavier <juniorxavier606@gmail.com>
2019-01-31 17:26:51 -0200

    Add font

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Home from "./screens/Home/home";
+import "./App.css";
 
 class App extends Component {
   render() {
--- frontend/src/components/Inicial/inicial.jsx
+++ frontend/src/components/Inicial/inicial.jsx
@@ -4,17 +4,12 @@ class PaginaInicial extends Component {
   render() {
     return (
       <React.Fragment>
-        <header className="masthead bg-primary text-white text-center">
+        <header className="masthead text-center">
           <div className="container">
-            <img
-              className="img-fluid mb-5 d-block mx-auto"
-              src={require("./logo.png")}
-              alt=""
-            />
             <h1 className="text-uppercase mb-0">Loja do Açaí</h1>
             <hr className="star-light" />
             <h2 className="font-weight-light mb-0">
-              Web Developer - Graphic Artist - User Experience Designer
+              Açaí Natural - Sorvetes - Barcas
             </h2>
           </div>
         </header>
